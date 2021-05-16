93ffcfb62a3a6e5d4d0a270f85f339bb063b3ae1
sendoru <sendol39@gmail.com>
2021-05-16 23:28:10 +0900

    2021/05/16

---
+++ 1208.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, s;
+	cin >> n, s;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	int min = v[0];
+
+	for (int i = 0; i < n; i++)
+	{
+
+	}
+}
